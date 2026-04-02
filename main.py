352ca4bd522926d2d87ec405b9cb427a5ea08e57
saulaliarana-creator <saulaliarana@gmail.com>
2026-04-01 21:30:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ app = FastAPI()
 def home():
     return {"respuesta": "servidor eclipse en linea"}
 
+accion_actual = "encender_led"
+
 @app.get("/comando")
 def comando():
-    return {"accion": "encender_led"}
+    return {"accion": accion_actual}
+
+@app.get("/encender")
+def encender():
+    global accion_actual
+    accion_actual = "encender_led"
+    return {"status": "LED encendido"}
+
+@app.get("/apagar")
+def apagar():
+    global accion_actual
+    accion_actual = "apagar_led"
+    return {"status": "LED apagado"}
