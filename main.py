782363f1adf025368e3c617974d3ef2071a5f32a
saulaliarana-creator <saulaliarana@gmail.com>
2026-04-02 13:34:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+
+accion_actual = "apagar_led"
 
 @app.get("/")
 def home():
     return {"respuesta": "servidor eclipse en linea"}
-
-accion_actual = "encender_led"
 
 @app.get("/comando")
 def comando():
@@ -23,3 +24,27 @@ def apagar():
     global accion_actual
     accion_actual = "apagar_led"
     return {"status": "LED apagado"}
+
+@app.get("/control", response_class=HTMLResponse)
+def control():
+    return """
+    <html>
+    <head>
+        <title>Proyecto Eclipse</title>
+    </head>
+    <body style="text-align:center; font-family:Arial;">
+        <h1>🌑 Proyecto Eclipse</h1>
+        <h2>Control de LED</h2>
+
+        <button onclick="fetch('/encender')" style="padding:20px; font-size:20px;">
+            ENCENDER
+        </button>
+
+        <br><br>
+
+        <button onclick="fetch('/apagar')" style="padding:20px; font-size:20px;">
+            APAGAR
+        </button>
+    </body>
+    </html>
+    """
